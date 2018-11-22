84c540e4697e684ce10434c2b20f2d36de0b5b4e
Ville <ville.hietanen@frantic.com>
2018-11-22 21:35:56 +0200

    departure and arrival fields min and max length

--- lib/database.d.ts
+++ lib/database.d.ts
@@ -4,3 +4,4 @@ import { IBookings } from './models/bookings';
 export declare const startDataBase: () => void;
 export declare const getBookingById: (id: mongoose.Types.ObjectId) => Promise<IBooking | null>;
 export declare const getBookings: () => Promise<IBookings[] | null>;
+export declare const getBookingsByUid: () => void;
--- src/models/flight.ts
+++ src/models/flight.ts
@@ -8,8 +8,8 @@ export interface IFlight extends mongoose.Document {
 }
 
 export const FlightSchema = new mongoose.Schema({
-    departure: {type: String, required: true},
-    arrival: {type: String, required: true},
+    departure: {type: String, required: true, minlength: 3, maxlength: 3},
+    arrival: {type: String, required: true, minlength: 3, maxlength: 3},
     departureDate: {type: Date, required: true},
     arrivalDate: {type: Date, required: true},
 });
